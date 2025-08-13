cfa0ddb943f22db59dd9a30156c5d6d7c0c794f0
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-08-13 15:01:02 -0400

    updated logic for scraping, added mongodb data storage

---
+++ GOTCHA.md
@@ -0,0 +1,23 @@
+# Quality of Life Gotchas for future reference
+
+Pointing mongodb to custom directories (uses /data/db by default.)
+```
+# 1) Make a data dir you own
+mkdir -p ~/mongodb/data
+chmod 700 ~/mongodb/data
+
+# 2) Start mongod and point it at that dir
+mongod --dbpath ~/mongodb/data
+
+# Leave that terminal running. In a new tab:
+mongosh
+
+# Mongosh interacive terminal
+test> use books_db
+switched to db books_db
+books_db> db.createCollection("books")
+{ ok: 1 }
+books_db> show collections
+books
+books_db>
+```
--- books/books/pipelines.py
+++ books/books/pipelines.py
@@ -4,9 +4,72 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# stdlib
+import hashlib
 
+# third party
+import pymongo
+from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+
+class MongoPipeline:
+    COLLECTION_NAME = "books"
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        """Fetches information from settings to return mongodb parameters."""
+        return cls(
+            mongo_uri=crawler.settings.get("MONGO_URI"),
+            mongo_db=crawler.settings.get("MONGO_DATABASE"),
+        )
+
+    def open_spider(self, spider):
+        """Opens connection to mongodb when spider starts."""
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        """Closes mongodb connection when spider is done."""
+        self.client.close()
+
+    def process_item(self, item, spider):
+        """
+        Inserts scraped items into mongodb, 
+        Only keeping one instance no matter how many times the scraperis run.
+        """
+        item_id = self.compute_item_id(item)
+        # Check to see if scraped item is unique
+        if self.db[self.COLLECTION_NAME].find_one({"_id": item_id}):
+            raise DropItem(f"Duplicate item found: {item}")
+        # If item is unique save to mongodb
+        else:
+            item["_id"] = item_id
+            self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
+            return item
+
+    # This is another method you can use that does not drop items when the url is changed
+    # both methods work but its up to your discretion which one you prefer
+    # this method uses the upsert=True flag to make sure unique items are kept
+    #def process_item(self, item, spider):
+    #    item_id = self.compute_item_id(item)
+    #    item_dict = ItemAdapter(item).asdict()
+
+    #    self.db[self.COLLECTION_NAME].update_one(
+    #        filter={"_id": item_id},
+    #        update={"$set": item_dict},
+    #        upsert=True
+    #    )
+
+        return item
+
+    def compute_item_id(self, item):
+        """Helper method to compute number of times an item is scraped."""
+        url = item["url"]
+        return hashlib.sha256(url.encode("utf-8")).hexdigest()
 
 class BooksPipeline:
     def process_item(self, item, spider):
